24d76330bdc3fb18cfd84c01e035c48db9f94e80
Kirill <K.katyshev1503@gmail.com>
2022-07-12 00:39:08 +0300

    Course_Work_1.2

--- src/Sky_Pro/Employee.java
+++ src/Sky_Pro/Employee.java
@@ -1,51 +1,55 @@
 package Sky_Pro;
 
 public class Employee {
-    public int department;
-    public String name;
-    public String lastName;
-    public String middleName;
-    public int salary;
-    public int id;
+    private int department;
+    private String name;
+    private String lastName;
+    private String middleName;
+    private int salary;
+    private int id;
     private static int counter;
 
-    public Employee (int department, String name, String lastName, String middleName, int salary) {
+    public Employee(int department, String name, String lastName, String middleName, int salary) {
         this.department = department;
         this.name = name;
         this.lastName = lastName;
         this.middleName = middleName;
         this.salary = salary;
-        this.id = getCounter();
-        counter++;
+        this.id = counter++;
     }
-    public int getDepartment() {
+
+    private int getDepartment() {
         return department;
     }
-    public void setDepartment(int department) {
+
+    private void setDepartment(int department) {
         this.department = department;
     }
+
     public String getName() {
         return name;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public String getMiddleName() {
         return middleName;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
-    }
-    public int getCounter() {
-        return counter;
     }
 
     public int getId() {
         return id;
     }
+
     public String toString() {
         return "Отдел № " + department + " ФИО: " + lastName + " " + name + " " + middleName + " Заработная плата: " +
                 salary + " рублей " + "id номер: " + id;
